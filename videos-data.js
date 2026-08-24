02559a5fe77504bcb42879c135e9a7bff9b7ffc0
luthf1-coder <luthfi1234321@gmail.com>
2026-08-24 21:36:50 +0700

    Update videos-data.js

--- videos-data.js
+++ videos-data.js
@@ -21,18 +21,6 @@ if (typeof firebase !== 'undefined' && !firebase.apps.length) {
 // Pusat Data Video
 const videoList = [
     {
-    "id": "kick_1787577705",
-    "title": "Stream via RTMP (OBS, Vmix, Zoom) with Restream",
-    "defaultViews": 0,
-    "thumb": "https://kick.com/favicon.ico",
-    "sources": [],
-    "driveEmbed": "https://drive.google.com/file/d/1PuHvLcwkHwy4LGRnfOfmHrSH3Ot2RzZl/preview",
-    "youtubeId": "",
-    "rumbleEmbed": "",
-    "downloadUrl": "https://drive.google.com/uc?export=download&id=1PuHvLcwkHwy4LGRnfOfmHrSH3Ot2RzZl",
-    "description": "VOD LS TV"
-},
-    {
     "id": "video8",
     "title": "VOD LIVE STREAMING BIOSKOP LIBURAN LUVIA STUDIO TV",
     "defaultViews": 0,
