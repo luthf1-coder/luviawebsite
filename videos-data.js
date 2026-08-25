612be20da39c29181cf39278c1834f7b8ec08623
luthf1-coder <luthfi1234321@gmail.com>
2026-08-25 11:06:45 +0700

    Auto-add new Kick VOD: Stream via RTMP (OBS, Vmix, Zoom) with Restream

--- videos-data.js
+++ videos-data.js
@@ -21,6 +21,18 @@ if (typeof firebase !== 'undefined' && !firebase.apps.length) {
 // Pusat Data Video
 const videoList = [
     {
+    "id": "kick_1787630786",
+    "title": "Stream via RTMP (OBS, Vmix, Zoom) with Restream",
+    "defaultViews": 0,
+    "thumb": "https://kick.com/favicon.ico",
+    "sources": [],
+    "driveEmbed": "https://drive.google.com/file/d/1CsSauvvlSRpI7bzjufUnl5Q-eLfehcAL/preview",
+    "youtubeId": "",
+    "rumbleEmbed": "",
+    "downloadUrl": "https://drive.google.com/uc?export=download&id=1CsSauvvlSRpI7bzjufUnl5Q-eLfehcAL",
+    "description": "VOD LS TV"
+},
+    {
     "id": "video8",
     "title": "VOD LIVE STREAMING BIOSKOP LIBURAN LUVIA STUDIO TV",
     "defaultViews": 0,
