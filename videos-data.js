63ba9cb84abad7c210232bf36b2649d2989956b1
luthf1-coder <luthfi1234321@gmail.com>
2026-08-26 08:01:44 +0700

    Auto-add new Kick VOD: Stream via RTMP (OBS, Vmix, Zoom) with Restream

--- videos-data.js
+++ videos-data.js
@@ -21,6 +21,18 @@ if (typeof firebase !== 'undefined' && !firebase.apps.length) {
 // Pusat Data Video
 const videoList = [
     {
+    "id": "kick_vod_123490654",
+    "title": "Stream via RTMP (OBS, Vmix, Zoom) with Restream",
+    "defaultViews": 0,
+    "thumb": "https://kick.com/favicon.ico",
+    "sources": [],
+    "driveEmbed": "https://drive.google.com/file/d/1GDOsTvewAmRzVIY1wyaZV2GdPk7-DMB4/preview",
+    "youtubeId": "",
+    "rumbleEmbed": "",
+    "downloadUrl": "https://drive.google.com/uc?export=download&id=1GDOsTvewAmRzVIY1wyaZV2GdPk7-DMB4",
+    "description": "VOD LS TV"
+},
+    {
     "id": "kick_1787680476",
     "title": "Stream via RTMP (OBS, Vmix, Zoom) with Restream",
     "defaultViews": 0,
